2e30f5e044267174cda8bcafe995fb18ff59e9ec
alejingutierrez <malgutierrezar@gmail.com>
2025-07-27 19:03:22 -0500

    fix(storybook): remove root override

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,25 +1,21 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-import { mergeConfig } from 'vite';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { mergeConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config = {
   framework: "@storybook/react-vite",
-  stories: [
-    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../src/**/*.docs.mdx",
-  ],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)", "../src/**/*.docs.mdx"],
   addons: ["@storybook/addon-essentials"],
   core: { disableTelemetry: true },
   async viteFinal(config) {
     return mergeConfig(config, {
-      root: path.resolve(__dirname, '..'),
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '../src'),
+          "@": path.resolve(__dirname, "../src"),
         },
       },
     });
