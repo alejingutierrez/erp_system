18c01af33d06abacfba75ff59659f91f9d1fdc44
alejingutierrez <malgutierrezar@gmail.com>
2025-07-27 19:04:11 -0500

    fix storybook config and module

---
+++ frontend/.storybook/main.cjs
@@ -0,0 +1,22 @@
+const { mergeConfig } = require('vite');
+const path = require('path');
+
+module.exports = {
+  framework: '@storybook/react-vite',
+  stories: [
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../src/**/*.docs.mdx',
+  ],
+  addons: ['@storybook/addon-essentials'],
+  core: { disableTelemetry: true },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      root: path.resolve(__dirname, '..'),
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+        },
+      },
+    });
+  },
+};
